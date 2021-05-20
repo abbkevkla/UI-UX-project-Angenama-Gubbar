763dfa4c69937e4bff518f0e9fe3ff14bb811ee8
abbjesjan <jesper.jansson@abbindustrigymnasium.se>
2021-05-20 12:25:14 +0200

    Update Login.vue

--- source/src/pages/Login.vue
+++ source/src/pages/Login.vue
@@ -1,30 +1,29 @@
 <template>
     <q-layout>
-        <q-page class="flex absolute-center" style="width: 60%;">
-            <div class="q-py-xl">
-                <div class="q-gutter-lg">
-                    <h2 class="q-py-xl text-center">Cafe Karma</h2>
-                    <div v-if="failedLogIn" class="row items-start">
-                        <q-card class="my-card text-negative bg-red-1" style="width: 100%;">
-                        <q-card-section>
-                            Felaktiga inloggningsuppgifter
-                        </q-card-section>
-                        </q-card>
-                    </div>
-                    <q-input  v-model="email" filled type="email" label="Email"/>
-                    <q-input  v-model="password" filled :type="isPwd ? 'password' : 'text'" label="Lösenord">
-                        <template v-slot:append>
-                        <q-icon
-                            :name="isPwd ? 'visibility_off' : 'visibility'"
-                            class="cursor-pointer"
-                            @click="isPwd = !isPwd"
-                        />
-                        </template>
-                    </q-input>
-                    <q-btn color="primary" label="Logga in" @click="login" style="width: 91%; height: 50px;"/>
-                </div>
+    <img style="object-fit: cover; height: 100vh; width: 100vw; opacity: 0.8" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg-s-msn-com.akamaized.net%2Ftenant%2Famp%2Fentityid%2FAACFQlr.img&f=1&nofb=1">   
+    <q-page class="flex absolute-center q-py-xl">
+        <div class="q-gutter-lg">
+            <h2 class="q-py-xl text-center text-white text-weight-medium">Café Karma</h2>
+            <div v-if="failedLogIn" class="row items-start">
+                <q-card class="my-card text-negative bg-red-1" style="width: 100%;">
+                <q-card-section>
+                    Felaktiga inloggningsuppgifter
+                </q-card-section>
+                </q-card>
             </div>
-        </q-page>
+            <q-input bg-color="secondary" v-model="email" filled type="email" label="Email"/>
+            <q-input bg-color="secondary" v-model="password" filled :type="isPwd ? 'password' : 'text'" label="Lösenord">
+                <template v-slot:append>
+                <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPwd = !isPwd"
+                />
+                </template>
+            </q-input>
+            <q-btn color="primary" label="Logga in" @click="login" style="width: 92%; height: 50px;"/>
+        </div>
+    </q-page>
     </q-layout>
 </template>
 
