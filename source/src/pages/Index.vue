8e294f861148c59d7bac4e22fc5eb807f2b58703
Samuel Olsson <43879730+abbsamols@users.noreply.github.com>
2021-05-20 10:12:50 +0200

    .

--- source/src/pages/Index.vue
+++ source/src/pages/Index.vue
@@ -1,9 +1,11 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <div class="fixed-top">
+      <q-img 
+        src="../assets/temple.jpg"
+        style="height: 550px; width:400px"
+      />
+    </div>
   </q-page>
 </template>
 
