d6039f44a3b8820f67bb7b7b89ec246eef49c36f
Samuel Olsson <43879730+abbsamols@users.noreply.github.com>
2021-05-20 11:02:52 +0200

    Update Index.vue

--- source/src/pages/Index.vue
+++ source/src/pages/Index.vue
@@ -1,11 +1,29 @@
 <template>
   <q-page class="flex flex-center">
     <div class="fixed-top">
+      <h4 style="text-align: right"> “Den vise bygger inget hopp om framtiden och ångrar ingenting i det förflutna.”</h4>
       <q-img 
         src="../assets/temple.jpg"
         style="height: 550px; width:400px"
       />
-      <h4 style="text-align: right"> “Den vise bygger inget hopp om framtiden och ångrar ingenting i det förflutna.”</h4>
+      <q-item class="q-ma-md" >
+        <q-btn color="primary">
+          <div style="width: 100%" class="text-center text-capitalize">
+            <div class="text-h5 text-weight-medium">
+              Se bakverk
+            </div>
+          </div>
+        </q-btn>
+      </q-item>
+      <q-item class="q-ma-md">
+        <q-btn color="primary">
+          <div style="width: 100%" class="text-center text-capitalize">
+            <div class="text-h5 text-weight-medium">
+              Om oss
+            </div>
+          </div>
+        </q-btn>
+      </q-item>
     </div>
   </q-page>
 </template>
