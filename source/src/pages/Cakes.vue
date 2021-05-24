0add3f14342f17388413e5dc1d8a2a0950b104a5
abbkevkla <43879859+abbkevkla@users.noreply.github.com>
2021-05-24 15:43:54 +0200

    grejor

--- source/src/pages/Cakes.vue
+++ source/src/pages/Cakes.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>
+  <div style="background-color: #f6edbe">
     <div>
-        <div class="text-h4 text-center text-weight-medium q-pt-lg">
-            Bakverk
+        <div class="row q-pt-lg">
+            <q-btn round dense flat icon="arrow_left" size="md" @click="redirect('home')"/>
+            <div class="text-h4 text-center text-weight-medium">
+                Bakverk
+            </div>
         </div>
         <div class="text-h6 text-center text-weight-light q-py-md q-px-md" style="border-bottom-style: solid;">
             Vi erbjuder helt nybakade bakverk gjorda på uteslutande ekologiska råvaror närproducerade i Västmanland. 
         </div>
     </div>
       <div style="height: 65vh" class="list scroll q-py-md">
+          <q-dialog v-model="prompt">
+               <cake-info-component :cakeInfo="cakeData"></cake-info-component>
+          </q-dialog>
           <q-item v-for="(item, index) in products" :key="index" class="q-ma-md">
-            <q-btn color="primary">
+            <q-btn color="primary" @click="ActivatePopup(item)">
                 <div style="width: 50%;">
                     <img :src="item.image" width="100%" height="150px" class="q-my-sm">
                 </div>
@@ -30,10 +36,18 @@
 </template>
 
 <script>
+import CakeInfoComponent from '../components/CakeInfoComponent.vue'
+
 export default {
+    components: {
+        CakeInfoComponent
+    },
+
     data () {
         return {
-            products: []
+            products: [],
+            prompt: false,
+            cakeData: {}
         }
     },
 
@@ -44,6 +58,18 @@ export default {
                 console.log(data);
                 this.products = data;
             })
+        },
+
+    methods: {
+        ActivatePopup (newData) {
+            this.prompt = true;
+            this.cakeData = newData;
+            console.log("Prompt activated")
+            console.log(this.cakeData)
+            },
+        redirect (address) {
+            this.$router.push(address)
+        }
         }
     }
 </script>
