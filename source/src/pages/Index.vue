3e9383ff649be4219197426056cf16f8905842f3
Samuel Olsson <43879730+abbsamols@users.noreply.github.com>
2021-05-25 09:32:29 +0200

    Update Index.vue

--- source/src/pages/Index.vue
+++ source/src/pages/Index.vue
@@ -1,12 +1,17 @@
 <template>
   <q-page class="flex flex-center">
     <div class="fixed-top">
-      <h4 style="text-align: right"> “Den vise bygger inget hopp om framtiden och ångrar ingenting i det förflutna.”</h4>
       <q-img 
         src="../assets/temple.jpg"
         style="height: 550px; width:400px"
       />
-      <q-item class="q-ma-md" >
+    </div>
+    <div class="absolute-right q-pa-sm">
+      <q-btn color="white" text-color="black" label="Logga ut" class="submit button" @click="signout"/>
+      <h5 style="text-align: right"> “Den vise bygger inget hopp om framtiden och ångrar ingenting i det förflutna.”</h5>
+    </div>
+    <div class="fixed-bottom">
+      <q-item class="q-ma-md">
         <q-btn color="primary">
           <div style="width: 100%" class="text-center text-capitalize">
             <div class="text-h5 text-weight-medium">
