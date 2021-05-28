b6e9e979365915c0a9dc3b5282ba4ccf0284c6f3
Samuel Olsson <43879730+abbsamols@users.noreply.github.com>
2021-05-28 11:35:34 +0200

    Update Index.vue

--- source/src/pages/Index.vue
+++ source/src/pages/Index.vue
@@ -1,31 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center bg-secondary">
     <div class="fixed-top">
       <q-img 
         src="../assets/temple.jpg"
         style="height: 550px; width:400px"
       />
     </div>
-    <div class="absolute-right q-pa-sm">
+    <div class="absolute-left q-pa-sm">
       <q-btn color="white" text-color="black" label="Logga ut" class="submit button" @click="logout('/')"/>
-      <h5 style="text-align: right"> “Den vise bygger inget hopp om framtiden och ångrar ingenting i det förflutna.”</h5>
     </div>
-    <div class="fixed-bottom">
+    <div class="absolute-right q-pa-sm">
+      <h4 class="color: white;" style="text-align: right; max-width: 210px; color: white;"> “Den vise bygger inget hopp om framtiden och ångrar ingenting i det förflutna.”</h4>
+    </div>
+    <div class="fixed-bottom q-pa-xl">
       <q-item class="q-ma-md">
-        <q-btn color="primary" @click="redirect('cakes')">
-          <div style="width: 100%" class="text-center text-capitalize">
-            <div class="text-h5 text-weight-medium">
-              Se bakverk
-            </div>
+        <q-btn class="q-mx-auto" color="primary" @click="redirect('cakes')">
+          <div class="text-h4 text-weight-medium text-center text-capitalize">
+            Se bakverk
           </div>
         </q-btn>
       </q-item>
       <q-item class="q-ma-md">
-        <q-btn color="primary" @click="redirect('about')">
-          <div style="width: 100%" class="text-center text-capitalize">
-            <div class="text-h5 text-weight-medium">
-              Om oss
-            </div>
+        <q-btn class="q-mx-auto" color="primary" @click="redirect('about')">
+          <div class="text-h5 text-weight-medium text-center text-capitalize">
+            Om oss
           </div>
         </q-btn>
       </q-item>
